7732bb19369dc7203819c3d5d541bc029dc0f737
Chris Picchione <jeff.picchione@gmail.com>
2021-04-11 08:49:58 -0500

    new logic to show date and time from custom module

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+var http = require('http');
+var dt = require('./myfirstmodule')
 
 app.get('/', (req, res) => res.send('Hello Jeffrey Again!!'));
+
+/* added following from node.js tutorial with http and dt vars)*/
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("<font color=red>The date and time are currently:  " + dt.myDateTime()+"</p>");
+res.end
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
