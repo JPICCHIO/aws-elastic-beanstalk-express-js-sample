cffea170e2360cd7d6f721d78d557f00f573a08e
Chris Picchione <jeff.picchione@gmail.com>
2021-04-23 01:07:21 -0500

    change text to hello world

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 
-app.get('/', (req, res) => res.send('Hello Jeffrey ONE MORE TIME!!'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
